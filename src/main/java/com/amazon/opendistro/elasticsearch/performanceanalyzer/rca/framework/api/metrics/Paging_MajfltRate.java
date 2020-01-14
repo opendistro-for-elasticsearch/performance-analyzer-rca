7768878294a6bf9497d31e75587e9e9231016ef7
Ruizhen <ruizhen@amazon.com>
2020-01-13 16:09:16 -0800

    merged from master and fixed some conflicts

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/api/metrics/Paging_MajfltRate.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/api/metrics/Paging_MajfltRate.java
@@ -20,6 +20,6 @@ import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api
 
 public class Paging_MajfltRate extends Metric {
   public Paging_MajfltRate(long evaluationIntervalSeconds) {
-    super(AllMetrics.OSMetrics.PAGING_MAJ_FLT_RATE.name(), evaluationIntervalSeconds);
+    super(AllMetrics.OSMetrics.PAGING_MAJ_FLT_RATE.toString(), evaluationIntervalSeconds);
   }
 }
