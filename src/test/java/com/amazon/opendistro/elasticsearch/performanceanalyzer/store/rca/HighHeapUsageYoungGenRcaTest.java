8e4c2691a4ab4d6df575f773f6dfcda4f7ff384a
Ruizhen <ruizhen@amazon.com>
2020-01-22 16:19:33 -0800

    Fix the exception in young gen rca unit test, add unit test for old gen rca

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/store/rca/HighHeapUsageYoungGenRcaTest.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/store/rca/HighHeapUsageYoungGenRcaTest.java
@@ -15,80 +15,93 @@
 
 package com.amazon.opendistro.elasticsearch.performanceanalyzer.store.rca;
 
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.GCType.OLD_GEN;
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.GCType.TOT_YOUNG_GC;
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.HeapDimension.MEM_TYPE;
+import static java.time.Instant.ofEpochMilli;
+
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metricsdb.MetricsDB;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.GradleTaskForRca;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.Metric;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.flow_units.ResourceFlowUnit;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.metrics.MetricTestHelper;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.store.rca.hotheap.HighHeapUsageYoungGenRca;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+
 
 @Category(GradleTaskForRca.class)
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({HighHeapUsageYoungGenRca.class})
-@PowerMockIgnore({"com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "org.w3c.*", "com.sun.org.apache.xalan.*"})
 public class HighHeapUsageYoungGenRcaTest {
-  private static final double CONVERT_BYTES_TO_MEGABYTES = Math.pow(1024, 2);
-  private static final int RCA_PERIOD = 12;
+  private static final double CONVERT_MEGABYTES_TO_BYTES = Math.pow(1024, 2);
   private MetricTestHelper heap_Used;
   private MetricTestHelper gc_Collection_Time;
-  private HighHeapUsageYoungGenRca youngGenRca;
+  private HighHeapUsageYoungGenRcaX youngGenRcaX;
   private List<String> columnName;
 
-  private void mockFlowUnits(int timeStampInSecond, double heapUsageVal, double gcCollectionTimeVal) {
-    Mockito.when(System.currentTimeMillis()).thenReturn(TimeUnit.SECONDS.toMillis(timeStampInSecond));
-    //generate empty flowunit and run operate enough times before evaluating RCA
-    heap_Used.setEmptyFlowUnitList();
-    gc_Collection_Time.setEmptyFlowUnitList();
-    //generate flowunit
-    heap_Used.createTestFlowUnits(columnName, Arrays.asList("OldGen", String.valueOf(heapUsageVal * CONVERT_BYTES_TO_MEGABYTES)));
-    gc_Collection_Time.createTestFlowUnits(columnName, Arrays.asList("totYoungGC", String.valueOf(gcCollectionTimeVal)));
+  /**
+   * generate flowunit and bind the flowunits it generate to metrics
+   */
+  private void mockFlowUnits(double heapUsageVal, double gcCollectionTimeVal) {
+    heap_Used.createTestFlowUnits(columnName, Arrays.asList(OLD_GEN.toString(), String.valueOf(heapUsageVal * CONVERT_MEGABYTES_TO_BYTES)));
+    gc_Collection_Time.createTestFlowUnits(columnName, Arrays.asList(TOT_YOUNG_GC.toString(), String.valueOf(gcCollectionTimeVal)));
   }
 
   @Before
   public void initTestHighHeapYoungGenRca() {
     heap_Used = new MetricTestHelper(5);
     gc_Collection_Time = new MetricTestHelper(5);
-    youngGenRca = new HighHeapUsageYoungGenRca(1, heap_Used, gc_Collection_Time);
-    columnName = Arrays.asList("MemType", "max");
-    PowerMockito.mockStatic(System.class);
+    youngGenRcaX = new HighHeapUsageYoungGenRcaX(1, heap_Used, gc_Collection_Time);
+    columnName = Arrays.asList(MEM_TYPE.toString(), MetricsDB.MAX);
   }
 
   @Test
   public void testHighHeapYoungGenRca() {
     ResourceFlowUnit flowUnit;
+    Clock constantClock = Clock.fixed(ofEpochMilli(0), ZoneId.systemDefault());
+
     //ts = 0, heap = 0, gc time = 0
-    mockFlowUnits(0, 0, 0);
-    flowUnit = youngGenRca.operate();
+    mockFlowUnits(0, 0);
+    youngGenRcaX.setClock(constantClock);
+    flowUnit = youngGenRcaX.operate();
     Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
 
     //ts = 1, heap = 450MB, gc time = 200ms
-    mockFlowUnits(1, 450, 200);
-    flowUnit = youngGenRca.operate();
+    mockFlowUnits(450, 200);
+    youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(1)));
+    flowUnit = youngGenRcaX.operate();
     Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
 
     //ts = 2, heap = 1050MB, gc time = 400ms
-    mockFlowUnits(2, 1050, 400);
-    flowUnit = youngGenRca.operate();
+    mockFlowUnits(1050, 400);
+    youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(2)));
+    flowUnit = youngGenRcaX.operate();
     Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
 
     //ts = 3, heap = 1550MB, gc time = 650ms
-    mockFlowUnits(3, 1550, 650);
-    flowUnit = youngGenRca.operate();
+    mockFlowUnits(1550, 650);
+    youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(3)));
+    flowUnit = youngGenRcaX.operate();
     Assert.assertTrue(flowUnit.getResourceContext().isUnhealthy());
   }
 
+  private static class HighHeapUsageYoungGenRcaX extends HighHeapUsageYoungGenRca {
+    public <M extends Metric> HighHeapUsageYoungGenRcaX(final int rcaPeriod,
+        final M heap_Used, final M gc_Collection_Time) {
+      super(rcaPeriod, heap_Used, gc_Collection_Time);
+    }
+
+    public void setClock(Clock testClock) {
+      this.clock = testClock;
+    }
+  }
 }
