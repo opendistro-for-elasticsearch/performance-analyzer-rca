7c7512d0e70bb6cc080525a152419ac5914fc80c
Joydeep Sinha <yojsyojs7@gmail.com>
2020-02-06 12:41:10 -0800

    Attenmpting a write to db file after creating a new db file. trying hard not to throw away data

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/persistence/PersistorBase.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/persistence/PersistorBase.java
@@ -55,6 +55,11 @@ public abstract class PersistorBase implements Persistable {
   private final FileRotate fileRotate;
   private final FileGC fileGC;
 
+  enum RotationType {
+    TRY_ROTATE,
+    FORCE_ROTATE
+  }
+
   PersistorBase(String dir, String filename, String dbProtocolString,
                 String storageFileRetentionCount, TimeUnit fileRotationTimeUnit,
                 long fileRotationPeriod) throws SQLException, IOException {
@@ -155,28 +160,64 @@ public abstract class PersistorBase implements Persistable {
       return;
     }
 
-    Path rotatedFile = fileRotate.tryRotate(System.currentTimeMillis());
-    if (rotatedFile != null) {
-      fileGC.eligibleForGc(rotatedFile.toFile().getName());
-      openNewDBFile();
-    }
+    rotateRegisterGarbageThenCreateNewDB(RotationType.TRY_ROTATE);
 
     try {
       writeFlowUnit(flowUnit, node.name());
     } catch (SQLException e) {
       LOG.error(
-          "RCA: Caught SQLException while writing flowuni.", e);
+          "RCA: Multiple attempts to write the data for table '{}' failed", node.name(), e);
+
+      // We rethrow this exception so that framework can take appropriate action.
+      throw e;
     }
   }
 
-  private synchronized <T extends ResourceFlowUnit> void writeFlowUnit(
-      T flowUnit, String tableName) throws SQLException {
+  private void rotateRegisterGarbageThenCreateNewDB(RotationType type) throws IOException, SQLException {
+    Path rotatedFile = null;
+    switch (type) {
+      case FORCE_ROTATE:
+        rotatedFile = fileRotate.forceRotate(System.currentTimeMillis());
+        break;
+      case TRY_ROTATE:
+        rotatedFile = fileRotate.tryRotate(System.currentTimeMillis());
+        break;
+    }
+    if (rotatedFile != null) {
+      fileGC.eligibleForGc(rotatedFile.toFile().getName());
+      openNewDBFile();
+    }
+  }
+
+  /**
+   * Writing a flow unit can fail if the DB file does not exist or if it is corrupted. In such
+   * cases, we create a new file and attempt to write the data in the new file.
+   * @param flowUnit The flow unit to be persisted.
+   * @param tableName The name of the table the data is to be persisted in.
+   * @param <T> The Type of flowUnit.
+   * @throws SQLException This is thrown when the DB files does not exist or the schema is
+   *     corrupted.
+   * @throws IOException This is thrown if the attempt to create a new DB file fails.
+   */
+  private <T extends ResourceFlowUnit> void writeFlowUnit(
+      T flowUnit, String tableName) throws SQLException, IOException {
     try {
+        tryWriteFlowUnit(flowUnit, tableName);
+    } catch (SQLException e) {
+      LOG.info(
+          "RCA: Fail to write to table '{}', try creating a new DB", tableName);
+      rotateRegisterGarbageThenCreateNewDB(RotationType.FORCE_ROTATE);
+      tryWriteFlowUnit(flowUnit, tableName);
+    }
+  }
+
+  private <T extends ResourceFlowUnit> void tryWriteFlowUnit(
+          T flowUnit, String tableName) throws SQLException {
       if (!tableNames.contains(tableName)) {
         LOG.info(
-            "RCA: Table '{}' does not exist. Creating one with columns: {}",
-            tableName,
-            flowUnit.getSqlSchema());
+                "RCA: Table '{}' does not exist. Creating one with columns: {}",
+                tableName,
+                flowUnit.getSqlSchema());
         createTable(tableName, flowUnit.getSqlSchema());
         tableNames.add(tableName);
       }
@@ -184,21 +225,15 @@ public abstract class PersistorBase implements Persistable {
 
       if (flowUnit.hasResourceSummary()) {
         writeSummary(
-            flowUnit.getResourceSummary(),
-            tableName,
-            getPrimaryKeyColumnName(tableName),
-            lastPrimaryKey);
+                flowUnit.getResourceSummary(),
+                tableName,
+                getPrimaryKeyColumnName(tableName),
+                lastPrimaryKey);
       }
-    } catch (SQLException e) {
-      LOG.info(
-          "RCA: Fail to write into table '{}', try recreating the DB", tableName);
-      openNewDBFile();
-    }
   }
 
-
   /** recursively insert nested summary to sql tables */
-  private synchronized void writeSummary(
+  private void writeSummary(
       GenericSummary summary,
       String referenceTable,
       String referenceTablePrimaryKeyFieldName,
