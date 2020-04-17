fef7c62990b7675b5a78a451dcc181685dbd52eb
Sid Narayan <sid@signalfx.com>
2020-04-16 18:39:56 -0700

    Revert "Added unit tests for the rca.stats.emitters package (#132)"
    This reverts commit 209d4a9901e72acdb865c73298a008d2114712fa.

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/stats/collectors/SampleAggregatorTest.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/stats/collectors/SampleAggregatorTest.java
@@ -101,7 +101,6 @@ public class SampleAggregatorTest {
     PeriodicSamplers periodicSamplers =
         new PeriodicSamplers(
             sampleAggregator, Collections.singletonList(sampler), 10, TimeUnit.HOURS);
-    periodicSamplers.startHeartbeat();
 
     RcaStatsReporter reporter = new RcaStatsReporter(Collections.singletonList(sampleAggregator));
 
