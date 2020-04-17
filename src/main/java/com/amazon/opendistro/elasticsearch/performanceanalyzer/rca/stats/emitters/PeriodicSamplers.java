c6f43d0ad0fd45668f3e6a6b6237f4a17d1b1d78
Sid Narayan <sid@signalfx.com>
2020-04-17 12:26:07 -0700

    Revert "Added unit tests for the rca.stats.emitters package (#132)" (#145)
    This reverts commit 209d4a9901e72acdb865c73298a008d2114712fa.

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/stats/emitters/PeriodicSamplers.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/stats/emitters/PeriodicSamplers.java
@@ -18,76 +18,55 @@ package com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.stats.emitte
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.stats.collectors.SampleAggregator;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.util.List;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class PeriodicSamplers implements Runnable {
   private static final Logger LOG = LogManager.getLogger(PeriodicSamplers.class);
-
   private final SampleAggregator aggregator;
   private final List<ISampler> allSamplers;
-  private final ScheduledFuture<?> samplerHandle;
-  private final AtomicBoolean running = new AtomicBoolean(false);
+  private final ScheduledExecutorService executor;
 
-  public PeriodicSamplers(SampleAggregator aggregator, List<ISampler> samplers, long freq, TimeUnit timeUnit) {
+  ScheduledFuture<?> future;
+
+  public PeriodicSamplers(
+      SampleAggregator aggregator, List<ISampler> samplers, long freq, TimeUnit timeUnit) {
     this.aggregator = aggregator;
     this.allSamplers = samplers;
-    ScheduledExecutorService executor = Executors.newScheduledThreadPool(
+
+    this.executor =
+        Executors.newScheduledThreadPool(
             1, new ThreadFactoryBuilder().setNameFormat("resource-sampler-%d").build());
-    this.samplerHandle = executor.scheduleAtFixedRate(this, 0, freq, timeUnit);
+    this.future = this.executor.scheduleAtFixedRate(this, 0, freq, timeUnit);
+    startExceptionHandlingThread();
   }
 
   @Override
   public void run() {
     for (ISampler sampler : allSamplers) {
-      try {
-        sampler.sample(aggregator);
-      } catch (Exception ex) {
-        LOG.error("Sampler {} encountered an exception during sampling", sampler.getClass().getSimpleName(), ex);
-      }
+      sampler.sample(aggregator);
     }
   }
 
-  public ScheduledFuture<?> getSamplerHandle() {
-    return samplerHandle;
-  }
-
-  /**
-   * Does nothing until the scheduled task referenced by {@link PeriodicSamplers#samplerHandle} is cancelled.
-   * Once the task is cancelled, this function logs a message throws an {@link InterruptedException} which should
-   * be handled by the parent thread.
-   */
-  void heartbeat() throws InterruptedException {
-    if (samplerHandle.isCancelled()) {
-      LOG.info("Periodic sampler cancellation requested.");
-      throw new InterruptedException();
-    }
-    Thread.sleep(1000L);
-  }
-
-  /**
-   * Starts and returns a {@link Thread} which performs a "heartbeat" check on the start of the
-   * {@link PeriodicSamplers#samplerHandle} every 1s
-   * @return The {@link Thread} which is performing the heartbeat
-   */
-  public Thread startHeartbeat() {
-    Thread t = new Thread(() -> {
-      running.set(true);
-      while (running.get()) {
-        try {
-          heartbeat();
-        } catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
-        }
-      }
-    });
-    t.start();
-    return t;
+  private void startExceptionHandlingThread() {
+    new Thread(
+            () -> {
+              while (true) {
+                try {
+                  future.get();
+                } catch (CancellationException cex) {
+                  LOG.info("Periodic sampler cancellation requested.");
+                } catch (Exception ex) {
+                  LOG.error("Resource state poller exception cause : {}", ex.getCause());
+                  ex.printStackTrace();
+                }
+              }
+            })
+        .start();
   }
 }
