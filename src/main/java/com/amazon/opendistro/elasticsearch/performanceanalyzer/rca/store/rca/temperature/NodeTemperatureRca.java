1636efebe6a71cfe1dea31986a5ba40d5e91a8ee
Karthik Kumarguru <52506191+ktkrg@users.noreply.github.com>
2020-04-29 14:38:32 -0700

    Add temperature profile along heap allocation rate dimension with no formatting changes (#170)

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/store/rca/temperature/NodeTemperatureRca.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/store/rca/temperature/NodeTemperatureRca.java
@@ -26,6 +26,7 @@ import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.temperature.NodeLevelDimensionalSummary;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.scheduler.FlowUnitOperationArgWrapper;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.store.rca.temperature.dimension.CpuUtilDimensionTemperatureRca;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.store.rca.temperature.dimension.HeapAllocRateTemperatureRca;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader.ClusterDetailsEventProcessor;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,71 +34,92 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class NodeTemperatureRca extends Rca<CompactNodeTemperatureFlowUnit> {
-    private final CpuUtilDimensionTemperatureRca cpuUtilDimensionTemperatureRca;
-    private static final Logger LOG = LogManager.getLogger(NodeTemperatureRca.class);
-    //private final HeapAllocHeatRca heapAllocHeatRca;
 
-    public NodeTemperatureRca(CpuUtilDimensionTemperatureRca cpuUtilDimensionTemperatureRca) {
-        super(5);
-        this.cpuUtilDimensionTemperatureRca = cpuUtilDimensionTemperatureRca;
-        //this.heapAllocHeatRca = heapAllocHeatRca;
+  private static final Logger LOG = LogManager.getLogger(NodeTemperatureRca.class);
+  private final CpuUtilDimensionTemperatureRca cpuUtilDimensionTemperatureRca;
+  private final HeapAllocRateTemperatureRca heapAllocRateTemperatureRca;
+
+  public NodeTemperatureRca(CpuUtilDimensionTemperatureRca cpuUtilDimensionTemperatureRca,
+      HeapAllocRateTemperatureRca heapAllocRateTemperatureRca) {
+    super(5);
+    this.cpuUtilDimensionTemperatureRca = cpuUtilDimensionTemperatureRca;
+    this.heapAllocRateTemperatureRca = heapAllocRateTemperatureRca;
+  }
+
+  @Override
+  public void generateFlowUnitListFromWire(FlowUnitOperationArgWrapper args) {
+    final List<FlowUnitMessage> flowUnitMessages =
+        args.getWireHopper().readFromWire(args.getNode());
+    List<CompactNodeTemperatureFlowUnit> flowUnitList = new ArrayList<>();
+    LOG.debug("rca: Executing fromWire: {}", this.getClass().getSimpleName());
+    for (FlowUnitMessage flowUnitMessage : flowUnitMessages) {
+      flowUnitList.add(CompactNodeTemperatureFlowUnit.buildFlowUnitFromWrapper(flowUnitMessage));
+    }
+    setFlowUnits(flowUnitList);
+  }
+
+  /**
+   * The goal of the NodeHeatRca is to build a node level temperature profile.
+   *
+   * <p>This is done by accumulating the {@code DimensionalFlowUnit} s it receives from the
+   * individual ResourceHeatRcas. The temperature profile build here is sent to the elected master
+   * node where this is used to calculate the cluster temperature profile.
+   *
+   * @return
+   */
+  @Override
+  public CompactNodeTemperatureFlowUnit operate() {
+    // TODO: Make this process a list of dimensions instead of writing them out and add the
+    //  processed dimension profiles to the summary.
+    List<DimensionalTemperatureFlowUnit> cpuFlowUnits = cpuUtilDimensionTemperatureRca
+        .getFlowUnits();
+    List<DimensionalTemperatureFlowUnit> heapAllocRateFlowUnits = heapAllocRateTemperatureRca
+        .getFlowUnits();
+    // EachResourceLevelHeat RCA should generate a one @{code DimensionalFlowUnit}.
+    if (cpuFlowUnits.size() != 1) {
+      throw new IllegalArgumentException("One flow unit expected. Found: " + cpuFlowUnits);
     }
 
-    @Override
-    public void generateFlowUnitListFromWire(FlowUnitOperationArgWrapper args) {
-        final List<FlowUnitMessage> flowUnitMessages =
-                args.getWireHopper().readFromWire(args.getNode());
-        List<CompactNodeTemperatureFlowUnit> flowUnitList = new ArrayList<>();
-        LOG.debug("rca: Executing fromWire: {}", this.getClass().getSimpleName());
-        for (FlowUnitMessage flowUnitMessage : flowUnitMessages) {
-            flowUnitList.add(CompactNodeTemperatureFlowUnit.buildFlowUnitFromWrapper(flowUnitMessage));
-        }
-        setFlowUnits(flowUnitList);
+    if (heapAllocRateFlowUnits.size() != 1) {
+      throw new IllegalStateException("One flow unit expected. Found: " + heapAllocRateFlowUnits);
     }
 
-    /**
-     * The goal of the NodeHeatRca is to build a node level temperature profile.
-     *
-     * <p>This is done by accumulating the {@code DimensionalFlowUnit} s it receives from the
-     * individual ResourceHeatRcas. The temperature profile build here is sent to the elected master
-     * node where this is used to calculate the cluster temperature profile.
-     *
-     * @return
-     */
-    @Override
-    public CompactNodeTemperatureFlowUnit operate() {
-        List<DimensionalTemperatureFlowUnit> cpuFlowUnits = cpuUtilDimensionTemperatureRca.getFlowUnits();
-        // EachResourceLevelHeat RCA should generate a one @{code DimensionalFlowUnit}.
-        if (cpuFlowUnits.size() != 1) {
-            throw new IllegalArgumentException("One flow unit expected. Found: " + cpuFlowUnits);
-        }
-
-        // This means that the input RCA didn't calculate anything. We can move on as well.
-        if (cpuFlowUnits.get(0).isEmpty()) {
-            return new CompactNodeTemperatureFlowUnit(System.currentTimeMillis());
-        }
-
-        List<NodeLevelDimensionalSummary> nodeDimensionProfiles = new ArrayList<>();
-        nodeDimensionProfiles.add(cpuFlowUnits.get(0).getNodeDimensionProfile());
-        FullNodeTemperatureSummary nodeProfile = buildNodeProfile(nodeDimensionProfiles);
-
-        ResourceContext resourceContext = new ResourceContext(Resources.State.UNKNOWN);
-        CompactNodeSummary summary = new CompactNodeSummary(nodeProfile.getNodeId(),
-                    nodeProfile.getHostAddress());
-        summary.fillFromNodeProfile(nodeProfile);
-
-        return new CompactNodeTemperatureFlowUnit(System.currentTimeMillis(), resourceContext, summary,
-                true);
+    // This means that the input RCA didn't calculate anything. We can move on as well.
+    if (cpuFlowUnits.get(0).isEmpty() && heapAllocRateFlowUnits.get(0).isEmpty()) {
+      return new CompactNodeTemperatureFlowUnit(System.currentTimeMillis());
     }
 
-    private FullNodeTemperatureSummary buildNodeProfile(List<NodeLevelDimensionalSummary> dimensionProfiles) {
-        ClusterDetailsEventProcessor.NodeDetails currentNodeDetails =
-                ClusterDetailsEventProcessor.getCurrentNodeDetails();
-        FullNodeTemperatureSummary nodeProfile = new FullNodeTemperatureSummary(currentNodeDetails.getId(),
-                currentNodeDetails.getHostAddress());
-        for (NodeLevelDimensionalSummary profile : dimensionProfiles) {
-            nodeProfile.updateNodeDimensionProfile(profile);
-        }
-        return nodeProfile;
+    List<NodeLevelDimensionalSummary> nodeDimensionProfiles = new ArrayList<>();
+    if (!cpuFlowUnits.get(0).isEmpty()) {
+      nodeDimensionProfiles.add(cpuFlowUnits.get(0).getNodeDimensionProfile());
     }
+
+    if (!heapAllocRateFlowUnits.get(0).isEmpty()) {
+      nodeDimensionProfiles.add(heapAllocRateFlowUnits.get(0).getNodeDimensionProfile());
+    }
+    FullNodeTemperatureSummary nodeProfile = buildNodeProfile(nodeDimensionProfiles);
+
+    ResourceContext resourceContext = new ResourceContext(Resources.State.UNKNOWN);
+    CompactNodeSummary summary = new CompactNodeSummary(nodeProfile.getNodeId(),
+        nodeProfile.getHostAddress());
+    summary.fillFromNodeProfile(nodeProfile);
+
+    return new CompactNodeTemperatureFlowUnit(
+        System.currentTimeMillis(), resourceContext,
+        summary,
+        true);
+  }
+
+  private FullNodeTemperatureSummary buildNodeProfile(
+      List<NodeLevelDimensionalSummary> dimensionProfiles) {
+    ClusterDetailsEventProcessor.NodeDetails currentNodeDetails =
+        ClusterDetailsEventProcessor.getCurrentNodeDetails();
+    FullNodeTemperatureSummary nodeProfile = new FullNodeTemperatureSummary(
+        currentNodeDetails.getId(),
+        currentNodeDetails.getHostAddress());
+    for (NodeLevelDimensionalSummary profile : dimensionProfiles) {
+      nodeProfile.updateNodeDimensionProfile(profile);
+    }
+    return nodeProfile;
+  }
 }
