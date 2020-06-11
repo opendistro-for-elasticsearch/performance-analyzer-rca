b21b3db256f711364d413fdb79a7afdfd4bbb45d
Ruizhen <ruizhen@amazon.com>
2020-06-11 16:50:17 -0700

    Add queue rejection RCA and UT

---
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/store/rca/threadpool/QueueRejectionRca.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.store.rca.threadpool;
+
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.ThreadPoolDimension.THREAD_POOL_TYPE;
+
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.grpc.FlowUnitMessage;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.grpc.ResourceType;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.grpc.ThreadPoolEnum;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.ThreadPoolType;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metricsdb.MetricsDB;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.Metric;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.Rca;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.Resources;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.contexts.ResourceContext;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.flow_units.MetricFlowUnit;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.flow_units.ResourceFlowUnit;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.persist.SQLParsingUtil;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.HotNodeSummary;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.HotResourceSummary;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.scheduler.FlowUnitOperationArgWrapper;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader.ClusterDetailsEventProcessor;
+import com.google.common.annotations.VisibleForTesting;
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * This RCA reads ThreadPool_RejectionReqs from upstream metrics and it keeps track of
+ * the amount of the time period(tp) when requests in the queues are continuously being rejected.
+ * This RCA is marked as unhealthy if tp we find in write/search queue is above the threshold.
+ */
+public class QueueRejectionRca extends Rca<ResourceFlowUnit<HotNodeSummary>> {
+  private static final Logger LOG = LogManager.getLogger(QueueRejectionRca.class);
+  private static final long REJECTION_TIME_PERIOD_IN_MILLISECOND = TimeUnit.SECONDS.toMillis(300);
+  private final int rcaPeriod;
+  private final List<QueueRejectionCollector> queueRejectionCollectors;
+  private int counter;
+  protected Clock clock;
+
+  public <M extends Metric> QueueRejectionRca(final int rcaPeriod, M threadPool_RejectedReqs) {
+    super(5);
+    this.rcaPeriod = rcaPeriod;
+    counter = 0;
+    clock = Clock.systemUTC();
+    queueRejectionCollectors = Collections.unmodifiableList(Arrays.asList(
+        new QueueRejectionCollector(ThreadPoolEnum.WRITE_QUEUE, ThreadPoolType.WRITE,
+            threadPool_RejectedReqs, REJECTION_TIME_PERIOD_IN_MILLISECOND),
+        new QueueRejectionCollector(ThreadPoolEnum.SEARCH_QUEUE, ThreadPoolType.SEARCH,
+            threadPool_RejectedReqs, REJECTION_TIME_PERIOD_IN_MILLISECOND)
+    ));
+  }
+
+  @VisibleForTesting
+  public void setClock(Clock clock) {
+    this.clock = clock;
+  }
+
+  @Override
+  public ResourceFlowUnit<HotNodeSummary> operate() {
+    counter += 1;
+    long currTimestamp = clock.millis();
+    if (counter == rcaPeriod) {
+      counter = 0;
+      ClusterDetailsEventProcessor.NodeDetails currentNode = ClusterDetailsEventProcessor
+          .getCurrentNodeDetails();
+      HotNodeSummary nodeSummary = null;
+      for (QueueRejectionCollector collector : queueRejectionCollectors) {
+        collector.collect(currTimestamp);
+        // if we've see thread pool rejection in the last 5 mins, the thread pool is considered as contended
+        if (collector.isUnhealthy(currTimestamp)) {
+          if (nodeSummary == null) {
+            nodeSummary = new HotNodeSummary(currentNode.getId(), currentNode.getHostAddress());
+          }
+          nodeSummary.addNestedSummaryList(collector.generateSummary(currTimestamp));
+        }
+      }
+      ResourceContext context;
+      if (nodeSummary == null) {
+        context = new ResourceContext(Resources.State.HEALTHY);
+      }
+      else {
+        context = new ResourceContext(Resources.State.UNHEALTHY);
+      }
+      boolean isDataNode = !currentNode.getIsMasterNode();
+      return new ResourceFlowUnit<>(currTimestamp, context, nodeSummary, isDataNode);
+    }
+    else {
+      return new ResourceFlowUnit<>(currTimestamp);
+    }
+  }
+
+  //TODO: move this method back into the Rca base class
+  @Override
+  public void generateFlowUnitListFromWire(FlowUnitOperationArgWrapper args) {
+    final List<FlowUnitMessage> flowUnitMessages =
+        args.getWireHopper().readFromWire(args.getNode());
+    List<ResourceFlowUnit<HotNodeSummary>> flowUnitList = new ArrayList<>();
+    LOG.debug("rca: Executing fromWire: {}", this.getClass().getSimpleName());
+    for (FlowUnitMessage flowUnitMessage : flowUnitMessages) {
+      flowUnitList.add(ResourceFlowUnit.buildFlowUnitFromWrapper(flowUnitMessage));
+    }
+    setFlowUnits(flowUnitList);
+  }
+
+  /**
+   * A collector class to collect rejection from each queue type
+   */
+  private static class QueueRejectionCollector {
+    private final ResourceType threadPool;
+    private final ThreadPoolType threadPoolMetric;
+    private final Metric threadPool_RejectedReqs;
+    private boolean hasRejection;
+    private long rejectionTimestamp;
+    private long rejectionTimePeriodThreshold;
+
+    public QueueRejectionCollector(final ThreadPoolEnum threadPool, final ThreadPoolType threadPoolMetric,
+        final Metric threadPool_RejectedReqs, final long threshold) {
+      this.threadPool = ResourceType.newBuilder().setThreadPool(threadPool).build();
+      this.threadPoolMetric = threadPoolMetric;
+      this.threadPool_RejectedReqs = threadPool_RejectedReqs;
+      this.hasRejection = false;
+      this.rejectionTimestamp = 0;
+      this.rejectionTimePeriodThreshold = threshold;
+    }
+
+    public void collect(final long currTimestamp) {
+      for (MetricFlowUnit flowUnit : threadPool_RejectedReqs.getFlowUnits()) {
+        if (flowUnit.isEmpty()) {
+          continue;
+        }
+        double rejectCnt = SQLParsingUtil.readDataFromSqlResult(flowUnit.getData(),
+            THREAD_POOL_TYPE.getField(), threadPoolMetric.toString(), MetricsDB.MAX);
+        if (!Double.isNaN(rejectCnt)) {
+          if (rejectCnt > 0) {
+            if (!hasRejection) {
+              rejectionTimestamp = currTimestamp;
+            }
+            hasRejection = true;
+          }
+          else {
+            hasRejection = false;
+          }
+        }
+        else {
+          LOG.error("Failed to parse metric from threadpool {}", threadPool.toString());
+        }
+      }
+    }
+
+    public boolean isUnhealthy(final long currTimestamp) {
+      return hasRejection && (currTimestamp - rejectionTimestamp) >= rejectionTimePeriodThreshold;
+    }
+
+    public HotResourceSummary generateSummary(final long currTimestamp) {
+      HotResourceSummary resourceSummary = null;
+      if (isUnhealthy(currTimestamp)) {
+        resourceSummary = new HotResourceSummary(threadPool,
+            TimeUnit.MILLISECONDS.toSeconds(rejectionTimePeriodThreshold),
+            TimeUnit.MILLISECONDS.toSeconds(currTimestamp - rejectionTimestamp),
+            0);
+      }
+      return resourceSummary;
+    }
+  }
+}
+
+
---
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/store/rca/threadpool/QueueRejectionRcaTest.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package com.amazon.opendistro.elasticsearch.performanceanalyzer.store.rca.threadpool;
+
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.ThreadPoolDimension.THREAD_POOL_TYPE;
+import static java.time.Instant.ofEpochMilli;
+
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.grpc.ThreadPoolEnum;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.ThreadPoolType;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metricsdb.MetricsDB;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.GradleTaskForRca;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.flow_units.ResourceFlowUnit;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.metrics.MetricTestHelper;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.HotNodeSummary;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.HotResourceSummary;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.store.rca.threadpool.QueueRejectionRca;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader.ClusterDetailsEventProcessorTestHelper;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(GradleTaskForRca.class)
+public class QueueRejectionRcaTest {
+
+  private MetricTestHelper threadPool_RejectedReqs;
+  private QueueRejectionRca queueRejectionRca;
+  private List<String> columnName;
+
+  /**
+   * generate flowunit and bind the flowunits it generate to metrics
+   */
+  private void mockFlowUnits(int writeRejectCnt, int searchRejectCnt) {
+    threadPool_RejectedReqs.createTestFlowUnitsWithMultipleRows(columnName,
+        Arrays.asList(
+            Arrays.asList(ThreadPoolType.WRITE.toString(), String.valueOf(writeRejectCnt)),
+            Arrays.asList(ThreadPoolType.SEARCH.toString(), String.valueOf(searchRejectCnt))
+        )
+    );
+  }
+
+  @Before
+  public void init() throws Exception {
+    threadPool_RejectedReqs = new MetricTestHelper(5);
+    queueRejectionRca = new QueueRejectionRca(1, threadPool_RejectedReqs);
+    columnName = Arrays.asList(THREAD_POOL_TYPE.toString(), MetricsDB.MAX);
+    ClusterDetailsEventProcessorTestHelper clusterDetailsEventProcessorTestHelper = new ClusterDetailsEventProcessorTestHelper();
+    clusterDetailsEventProcessorTestHelper.addNodeDetails("node1", "127.0.0.0", false);
+    clusterDetailsEventProcessorTestHelper.generateClusterDetailsEvent();
+  }
+
+  @Test
+  public void testWriteQueueOnly() {
+    ResourceFlowUnit<HotNodeSummary> flowUnit;
+    Clock constantClock = Clock.fixed(ofEpochMilli(0), ZoneId.systemDefault());
+
+    mockFlowUnits(0, 0);
+    queueRejectionRca.setClock(constantClock);
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+
+    mockFlowUnits(0, 0);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(3)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+
+    mockFlowUnits(1, 0);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(4)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+
+    mockFlowUnits(1, 0);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(7)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+
+    mockFlowUnits(1, 0);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(10)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertTrue(flowUnit.getResourceContext().isUnhealthy());
+
+    Assert.assertTrue(flowUnit.hasResourceSummary());
+    HotNodeSummary nodeSummary = flowUnit.getSummary();
+    Assert.assertEquals(1, nodeSummary.getNestedSummaryList().size());
+    Assert.assertTrue(nodeSummary.getNestedSummaryList().get(0) instanceof HotResourceSummary);
+    HotResourceSummary resourceSummary = (HotResourceSummary) nodeSummary.getNestedSummaryList().get(0);
+    Assert.assertEquals(ThreadPoolEnum.WRITE_QUEUE, resourceSummary.getResourceType().getThreadPool());
+    Assert.assertEquals(0.01, 6.0, resourceSummary.getValue());
+
+    mockFlowUnits(0, 0);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(12)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+  }
+
+  @Test
+  public void testWriteAndSearchQueues() {
+    ResourceFlowUnit<HotNodeSummary> flowUnit;
+    Clock constantClock = Clock.fixed(ofEpochMilli(0), ZoneId.systemDefault());
+
+    mockFlowUnits(0, 0);
+    queueRejectionRca.setClock(constantClock);
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+
+    mockFlowUnits(0, 1);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(3)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+
+    mockFlowUnits(1, 1);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(5)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+
+    mockFlowUnits(1, 1);
+    queueRejectionRca.setClock(Clock.offset(constantClock, Duration.ofMinutes(12)));
+    flowUnit = queueRejectionRca.operate();
+    Assert.assertTrue(flowUnit.getResourceContext().isUnhealthy());
+
+    Assert.assertTrue(flowUnit.hasResourceSummary());
+    HotNodeSummary nodeSummary = flowUnit.getSummary();
+    Assert.assertEquals(2, nodeSummary.getNestedSummaryList().size());
+    Assert.assertTrue(nodeSummary.getNestedSummaryList().get(0) instanceof HotResourceSummary);
+    HotResourceSummary resourceSummary = (HotResourceSummary) nodeSummary.getNestedSummaryList().get(1);
+    Assert.assertEquals(ThreadPoolEnum.SEARCH_QUEUE, resourceSummary.getResourceType().getThreadPool());
+    Assert.assertEquals(0.01, 9.0, resourceSummary.getValue());
+    resourceSummary = (HotResourceSummary) nodeSummary.getNestedSummaryList().get(0);
+    Assert.assertEquals(ThreadPoolEnum.WRITE_QUEUE, resourceSummary.getResourceType().getThreadPool());
+    Assert.assertEquals(0.01, 7.0, resourceSummary.getValue());
+  }
+}
