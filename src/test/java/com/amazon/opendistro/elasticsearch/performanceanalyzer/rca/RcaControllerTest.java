37be24362522aae4ffe3c15daef4bb361269c156
Karthik Kumarguru <kkumargu@amazon.com>
2020-07-19 15:55:05 -0700

    Common changes needed to support dynamic en/disabling of config overrides

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/RcaControllerTest.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/RcaControllerTest.java
@@ -39,6 +39,7 @@ import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import javax.swing.JSeparator;
 import org.jooq.tools.json.JSONObject;
 import org.junit.After;
 import org.junit.Assert;
@@ -359,8 +360,12 @@ public class RcaControllerTest {
   }
 
   private void setMyIp(String ip, AllMetrics.NodeRole nodeRole) {
+    final String separator = System.lineSeparator();
     JSONObject jtime = new JSONObject();
     jtime.put("current_time", 1566414001749L);
+
+    JSONObject jOverrides = new JSONObject();
+    JSONObject jOverridesTimeStamp = new JSONObject();
 
     JSONObject jNode = new JSONObject();
     jNode.put(AllMetrics.NodeDetailColumns.ID.toString(), "4sqG_APMQuaQwEW17_6zwg");
@@ -368,8 +373,16 @@ public class RcaControllerTest {
     jNode.put(AllMetrics.NodeDetailColumns.ROLE.toString(), nodeRole);
 
     ClusterDetailsEventProcessor eventProcessor = new ClusterDetailsEventProcessor();
+    StringBuilder nodeDetails = new StringBuilder();
+    nodeDetails.append(jtime);
+    nodeDetails.append(separator);
+    nodeDetails.append(jOverrides);
+    nodeDetails.append(separator);
+    nodeDetails.append(jOverridesTimeStamp);
+    nodeDetails.append(separator);
+    nodeDetails.append(jNode.toString());
     eventProcessor.processEvent(
-        new Event("", jtime.toString() + System.lineSeparator() + jNode.toString(), 0));
+        new Event("", nodeDetails.toString(), 0));
     rcaController.getAppContext().setClusterDetailsEventProcessor(eventProcessor);
   }
 
