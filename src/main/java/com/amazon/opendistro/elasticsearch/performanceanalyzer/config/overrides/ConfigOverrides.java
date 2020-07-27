a795e6d8ac1063942153193341c725830956da86
Karthik Kumarguru <kkumargu@amazon.com>
2020-07-27 09:28:11 -0700

    Add licence header to new files

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/config/overrides/ConfigOverrides.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/config/overrides/ConfigOverrides.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package com.amazon.opendistro.elasticsearch.performanceanalyzer.config.overrides;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * POJO for config overrides.
+ * POJO for config overrides. The class contains two sets of overrides, one for enabling and one
+ * for disabling.
  */
 public class ConfigOverrides {
     private Overrides enable;
@@ -31,6 +47,11 @@ public class ConfigOverrides {
         this.disable = disable;
     }
 
+    /**
+     * Class containing the overridable attributes of the system. Currently, overriding the
+     * enabled/disabled state for RCAs, deciders, and actions are supported. More attributes can
+     * be added as needed.
+     */
     public static class Overrides {
         private List<String> rcas;
         private List<String> deciders;
