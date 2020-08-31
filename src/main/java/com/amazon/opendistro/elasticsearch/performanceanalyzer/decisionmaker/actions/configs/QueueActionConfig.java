c820fb7f26f1cd2f4c3fba7f05258c0318e16188
Vigya Sharma <vigyaspeaks@gmail.com>
2020-08-31 01:55:10 -0700

    remove typo

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/decisionmaker/actions/configs/QueueActionConfig.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/decisionmaker/actions/configs/QueueActionConfig.java
@@ -28,7 +28,7 @@ import org.apache.logging.log4j.Logger;
 /**
  * Defines config for threadpool queue related actions
  *
- * <p>com.amazon.opendistro.elasticsearch.performanceanalyzer.collections.TimeExpiringSetTestConfigs are expected in the following json format:
+ * <p>Configs are expected in the following json format:
  * {
  *   "action-config-settings": {
  *     // Queue Capacity bounds are expressed as absolute queue size
