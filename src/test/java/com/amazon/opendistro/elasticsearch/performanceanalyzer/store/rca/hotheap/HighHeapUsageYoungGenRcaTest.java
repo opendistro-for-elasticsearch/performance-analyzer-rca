f440fc538fb795cc2f1e2c32ef5727a8abc0e54f
Sid Narayan <sidnaray@amazon.com>
2020-10-01 13:18:08 -0700

    Modify HighHeapUsageYoungGenRca to better detect sizing issues
    - Now takes full GC pause time into account when making decisions
    - Takes into account the amount of garbage mistakenly promoted into the
      old generation

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/store/rca/hotheap/HighHeapUsageYoungGenRcaTest.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/store/rca/hotheap/HighHeapUsageYoungGenRcaTest.java
@@ -16,8 +16,12 @@
 package com.amazon.opendistro.elasticsearch.performanceanalyzer.store.rca.hotheap;
 
 import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.GCType.OLD_GEN;
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.GCType.TOT_FULL_GC;
 import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.GCType.TOT_YOUNG_GC;
 import static com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics.HeapDimension.MEM_TYPE;
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.ResourceUtil.FULL_GC_PAUSE_TIME;
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.ResourceUtil.MINOR_GC_PAUSE_TIME;
+import static com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.ResourceUtil.YOUNG_GEN_PROMOTION_RATE;
 import static java.time.Instant.ofEpochMilli;
 
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.metricsdb.MetricsDB;
@@ -25,8 +29,10 @@ import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.GradleTaskFor
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.Metric;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.flow_units.ResourceFlowUnit;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.metrics.MetricTestHelper;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.api.summaries.HotResourceSummary;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.store.rca.hotheap.HighHeapUsageYoungGenRca;
 
+import com.google.common.collect.Lists;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.ZoneId;
@@ -49,11 +55,13 @@ public class HighHeapUsageYoungGenRcaTest {
   private List<String> columnName;
 
   /**
-   * generate flowunit and bind the flowunits it generate to metrics
+   * generates mock metric flow units that RCAs can consume
    */
-  private void mockFlowUnits(double heapUsageVal, double gcCollectionTimeVal) {
+  private void mockFlowUnits(double heapUsageVal, double minorGcTime, double fullGcTime) {
     heap_Used.createTestFlowUnits(columnName, Arrays.asList(OLD_GEN.toString(), String.valueOf(heapUsageVal * CONVERT_MEGABYTES_TO_BYTES)));
-    gc_Collection_Time.createTestFlowUnits(columnName, Arrays.asList(TOT_YOUNG_GC.toString(), String.valueOf(gcCollectionTimeVal)));
+    List<String> youngGcRow = Arrays.asList(TOT_YOUNG_GC.toString(), String.valueOf(minorGcTime));
+    List<String> fullGcRow = Arrays.asList(TOT_FULL_GC.toString(), String.valueOf(fullGcTime));
+    gc_Collection_Time.createTestFlowUnitsWithMultipleRows(columnName, Lists.newArrayList(youngGcRow, fullGcRow));
   }
 
   @Before
@@ -69,29 +77,57 @@ public class HighHeapUsageYoungGenRcaTest {
     ResourceFlowUnit flowUnit;
     Clock constantClock = Clock.fixed(ofEpochMilli(0), ZoneId.systemDefault());
 
-    //ts = 0, heap = 0, gc time = 0
-    mockFlowUnits(0, 0);
+    // ts = 0, heap = 0, minor gc time = 0, full gc time = 0ms
+    mockFlowUnits(0, 0, 0);
     youngGenRcaX.setClock(constantClock);
     flowUnit = youngGenRcaX.operate();
     Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
 
-    //ts = 1, heap = 450MB, gc time = 200ms
-    mockFlowUnits(450, 200);
+    // ts = 1, heap = 450MB, minor gc time = 200ms, full gc time = 9000ms
+    mockFlowUnits(450, 200, 9000);
     youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(1)));
     flowUnit = youngGenRcaX.operate();
     Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
 
-    //ts = 2, heap = 1050MB, gc time = 400ms
-    mockFlowUnits(1050, 400);
+    // ts = 2, heap = 1050MB, minor gc time = 240ms, full gc time = 12000ms
+    // the average full GC time is now 10.5s which is > the threshold of 10s
+    mockFlowUnits(1050, 400, 12000);
     youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(2)));
     flowUnit = youngGenRcaX.operate();
-    Assert.assertFalse(flowUnit.getResourceContext().isUnhealthy());
+    Assert.assertTrue(flowUnit.getResourceContext().isUnhealthy());
+    HotResourceSummary summary = (HotResourceSummary) flowUnit.getSummary();
+    Assert.assertEquals(FULL_GC_PAUSE_TIME, summary.getResource());
+    Assert.assertEquals(10500, summary.getValue(), 0.1);
 
-    //ts = 3, heap = 1550MB, gc time = 650ms
-    mockFlowUnits(1550, 650);
+    // ts = 3, heap = 1650MB, minor gc time = 600ms, full gc time = 0ms
+    // the average promotion rate is now 550 MB/s which is > the threshold of 500 MB/s
+    mockFlowUnits(1650, 600, 0);
     youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(3)));
     flowUnit = youngGenRcaX.operate();
+    summary = (HotResourceSummary) flowUnit.getSummary();
     Assert.assertTrue(flowUnit.getResourceContext().isUnhealthy());
+    Assert.assertEquals(YOUNG_GEN_PROMOTION_RATE, summary.getResource());
+    Assert.assertEquals(550, summary.getValue(), 0.1);
+
+    // ts = 4, heap = 1650MB, minor gc time = 800ms, full gc time = 0ms
+    // the average minor gc time is now 500 ms / sec which is > the threshold of 400 ms / sec
+    mockFlowUnits(1650, 800, 0);
+    youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(4)));
+    flowUnit = youngGenRcaX.operate();
+    summary = (HotResourceSummary) flowUnit.getSummary();
+    Assert.assertTrue(flowUnit.getResourceContext().isUnhealthy());
+    Assert.assertEquals(MINOR_GC_PAUSE_TIME, summary.getResource());
+    Assert.assertEquals(500, summary.getValue(), 0.1);
+
+    // ts = 5, heap = 0MB, minor gc time = 0ms, full gc time = 0ms
+    // the average garbage promotion percent is now 100% which is > the threshold of 80%
+    mockFlowUnits(0, 0, 0);
+    youngGenRcaX.setClock(Clock.offset(constantClock, Duration.ofSeconds(5)));
+    flowUnit = youngGenRcaX.operate();
+    summary = (HotResourceSummary) flowUnit.getSummary();
+    Assert.assertTrue(flowUnit.getResourceContext().isUnhealthy());
+    Assert.assertEquals(YOUNG_GEN_PROMOTION_RATE, summary.getResource());
+    Assert.assertEquals(1, summary.getValue(), 0.1);
   }
 
   private static class HighHeapUsageYoungGenRcaX extends HighHeapUsageYoungGenRca {
