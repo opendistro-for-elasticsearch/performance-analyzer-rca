5cf4bbe79d1df2442f9b15bd6006c3ce60f1c603
Karthik Kumarguru <kkumargu@amazon.com>
2020-10-01 12:16:07 -0700

    Add unit tests for GC snapshot and processor

---
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/reader/GarbageCollectorInfoProcessorTest.java
@@ -0,0 +1,69 @@
+package com.amazon.opendistro.elasticsearch.performanceanalyzer.reader;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.collectors.GCInfoCollector.GCInfo;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.PerformanceAnalyzerMetrics;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader.GarbageCollectorInfoSnapshot.Fields;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader_writer_shared.Event;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GarbageCollectorInfoProcessorTest {
+
+  private static final String DB_URL = "jdbc:sqlite:";
+  private static final String TEST_MEM_POOL = "testMemPool";
+  private static final String COLLECTOR_NAME = "testCollectorName";
+  private static final String GC_INFO_KEY = "gc_info";
+  private GarbageCollectorInfoProcessor gcProcessor;
+  private long currTimestamp;
+  private NavigableMap<Long, GarbageCollectorInfoSnapshot> snapMap;
+  Connection conn;
+
+  @Before
+  public void setup() throws Exception {
+    Class.forName("org.sqlite.JDBC");
+    System.setProperty("java.io.tmpdir", "/tmp");
+    conn = DriverManager.getConnection(DB_URL);
+    this.currTimestamp = System.currentTimeMillis();
+    this.snapMap = new TreeMap<>();
+    this.gcProcessor = GarbageCollectorInfoProcessor
+        .buildGarbageCollectorInfoProcessor(currTimestamp, conn, snapMap);
+  }
+
+  @Test
+  public void testHandleEvent() throws Exception {
+    Event testEvent = buildTestGcInfoEvent();
+
+    gcProcessor.initializeProcessing(currTimestamp, System.currentTimeMillis());
+
+    assertTrue(gcProcessor.shouldProcessEvent(testEvent));
+
+    gcProcessor.processEvent(testEvent);
+    gcProcessor.finalizeProcessing();
+
+    GarbageCollectorInfoSnapshot snap = snapMap.get(currTimestamp);
+    Result<Record> result = snap.fetchAll();
+    assertEquals(1, result.size());
+    assertEquals(TEST_MEM_POOL, result.get(0).get(Fields.MEMORY_POOL.toString()));
+    assertEquals(COLLECTOR_NAME, result.get(0).get(Fields.COLLECTOR_NAME.toString()));
+  }
+
+  private Event buildTestGcInfoEvent() {
+    StringBuilder val = new StringBuilder();
+    val.append(PerformanceAnalyzerMetrics.getJsonCurrentMilliSeconds())
+       .append(PerformanceAnalyzerMetrics.sMetricNewLineDelimitor);
+
+    val.append(new GCInfo(TEST_MEM_POOL, COLLECTOR_NAME).serialize())
+       .append(PerformanceAnalyzerMetrics.sMetricNewLineDelimitor);
+    return new Event(GC_INFO_KEY, val.toString(), System.currentTimeMillis());
+  }
+}
---
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/reader/GarbageCollectorInfoSnapshotTest.java
@@ -0,0 +1,54 @@
+package com.amazon.opendistro.elasticsearch.performanceanalyzer.reader;
+
+import static org.junit.Assert.assertEquals;
+
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader.GarbageCollectorInfoSnapshot.Fields;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.jooq.BatchBindStep;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GarbageCollectorInfoSnapshotTest {
+
+  private static final String DB_URL = "jdbc:sqlite:";
+  private static final String TEST_MEM_POOL = "OldGen";
+  private static final String COLLECTOR_NAME = "G1";
+  private Connection conn;
+  GarbageCollectorInfoSnapshot snapshot;
+
+  @Before
+  public void setup() throws Exception {
+    Class.forName("org.sqlite.JDBC");
+    System.setProperty("java.io.tmpdir", "/tmp");
+    conn = DriverManager.getConnection(DB_URL);
+    snapshot = new GarbageCollectorInfoSnapshot(conn, System.currentTimeMillis());
+  }
+
+  @Test
+  public void testReadGcSnapshot() throws Exception {
+    final BatchBindStep handle = snapshot.startBatchPut();
+    insertIntoTable(handle, TEST_MEM_POOL, COLLECTOR_NAME);
+
+    final Result<Record> result = snapshot.fetchAll();
+
+    assertEquals(1, result.size());
+    assertEquals(TEST_MEM_POOL, result.get(0).get(Fields.MEMORY_POOL.toString()));
+    assertEquals(COLLECTOR_NAME, result.get(0).get(Fields.COLLECTOR_NAME.toString()));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    conn.close();
+  }
+
+  private void insertIntoTable(BatchBindStep handle, String memPool, String collectorName) {
+    Object[] bindVals = new Object[2];
+    bindVals[0] = memPool;
+    bindVals[1] = collectorName;
+    handle.bind(bindVals).execute();
+  }
+}
