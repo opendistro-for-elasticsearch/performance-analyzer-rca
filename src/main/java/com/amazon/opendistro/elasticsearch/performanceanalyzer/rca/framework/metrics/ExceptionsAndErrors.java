10f05fb960c0294c686d1c6d43f5c97a8d979b26
Ricardo L. Stephen <43506361+ricardolstephen@users.noreply.github.com>
2020-10-12 15:08:35 -0700

    Add operational metrics for batch metrics api
    * READER_METRICSDB_ACCESS_ERRORS: errors accessing metricsdb files
    * METRICSDB_FILE_SIZE: size of generated metricsdb files
    * BATCH_METRICS_ENABLED: whether or not batch metrics is enabled
    * BATCH_METRICS_HTTP_CLIENT_ERROR: number of bad batch metrics requests
    * BATCH_METRICS_HTTP_HOST_ERROR: number of batch metrics requests where
        the host ran into errors
    * BATCH_METRICS_HTTP_SUCCESS: number of successful batch metrics
        requests
    * BATCH_METRICS_EXCEEDED_MAX_DATAPOINTS: requests where the user
        requested for more datapoints than the hardcoded limit of 100800
    * BATCH_METRICS_QUERY_PROCESSING_TIME: amount of time required to
        process batch metrics requests
    * BATCH_METRICS_NUM_METRICSDB_FILES: number of metricsdb files within
        the batch metrics retention period
    * BATCH_METRICS_DATA_SIZE: amount of data batch metrics is storing

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/metrics/ExceptionsAndErrors.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/metrics/ExceptionsAndErrors.java
@@ -42,7 +42,12 @@ public enum ExceptionsAndErrors implements MeasurementSet {
   /**
    * When persisting action or flowunits, the persistable throws an exception when it is unable to write to DB.
    */
-  EXCEPTION_IN_PERSIST("ExceptionInPersist", "namedCount", Statistics.NAMED_COUNTERS);
+  EXCEPTION_IN_PERSIST("ExceptionInPersist", "namedCount", Statistics.NAMED_COUNTERS),
+
+  /**
+   * When the reader encounters errors accessing metricsdb files.
+   */
+  READER_METRICSDB_ACCESS_ERRORS("ReaderMetricsdbAccessError");
 
   /** What we want to appear as the metric name. */
   private String name;
---
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/metrics/ReaderMetrics.java
@@ -0,0 +1,103 @@
+package com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.framework.metrics;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.stats.eval.Statistics;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.rca.stats.measurements.MeasurementSet;
+
+public enum ReaderMetrics implements MeasurementSet {
+
+    /**
+     * Size of generated metricsdb files.
+     */
+    METRICSDB_FILE_SIZE("MetricsdbFileSize", "bytes", Arrays.asList(Statistics.MAX, Statistics.MEAN)),
+
+    /**
+     * Whether or not batch metrics is enabled (0 for enabled, 1 for disabled).
+     */
+    BATCH_METRICS_ENABLED("BatchMetricsEnabled", "count", Statistics.SAMPLE),
+
+    /**
+     * Number of http requests where the client gave a bad request.
+     */
+    BATCH_METRICS_HTTP_CLIENT_ERROR("BatchMetricsHttpClientError", "count", Statistics.COUNT),
+
+    /**
+     * Number of http requests where the host could not generate a correct response.
+     */
+    BATCH_METRICS_HTTP_HOST_ERROR("BatchMetricsHttpHostError", "count", Statistics.COUNT),
+
+    /**
+     * Number of successful queries.
+     */
+    BATCH_METRICS_HTTP_SUCCESS("BatchMetricsHttpSuccess", "count", Statistics.COUNT),
+
+    /**
+     * Number of times a query for batch metrics exceeded the maximum number of requestable datapoints.
+     */
+    BATCH_METRICS_EXCEEDED_MAX_DATAPOINTS("ExceededBatchMetricsMaxDatapoints", "count", Statistics.COUNT),
+
+    /**
+     * Amount of time required to process valid batch metrics requests.
+     */
+    BATCH_METRICS_QUERY_PROCESSING_TIME("BatchMetricsQueryProcessingTime", "millis",
+        Arrays.asList(Statistics.MAX, Statistics.MEAN, Statistics.SUM)),
+
+    /**
+     * Number of metricsdb files associated with batch metrics.
+     */
+    BATCH_METRICS_NUM_METRICSDB_FILES("BatchMetricsNumMetricsdbFiles", "count",
+        Arrays.asList(Statistics.MAX, Statistics.MEAN)),
+
+    /**
+     * Amount of locally stored data within the batch metrics retention period.
+     */
+    BATCH_METRICS_DATA_SIZE("BatchMetricsDataSize", "bytes",
+        Arrays.asList(Statistics.MAX, Statistics.MEAN));
+
+    /** What we want to appear as the metric name. */
+    private String name;
+
+    /**
+     * The unit the measurement is in. This is not used for the statistics calculations but as an
+     * information that will be dumped with the metrics.
+     */
+    private String unit;
+
+    /**
+     * Multiple statistics can be collected for each measurement like MAX, MIN and MEAN. This is a
+     * collection of one or more such statistics.
+     */
+    private List<Statistics> statsList;
+
+    ReaderMetrics(String name, String unit, List<Statistics> stats) {
+        this.name = name;
+        this.unit = unit;
+        this.statsList = stats;
+    }
+
+    ReaderMetrics(String name, String unit, Statistics stats) {
+        this(name, unit, Collections.singletonList(stats));
+    }
+
+    public String toString() {
+        return new StringBuilder(name).append("-").append(unit).toString();
+    }
+
+    @Override
+    public List<Statistics> getStatsList() {
+        return statsList;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getUnit() {
+        return unit;
+    }
+}
