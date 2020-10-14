2bf2312861078e724aede3c4966c678176eae54c
Joydeep Sinha <yojsyojs7@gmail.com>
2020-10-13 19:50:29 -0700

    Log error message ignore

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/integTests/tests/queue_tuning/dedicated_master/QueueDeciderDedicatedMasterITest.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/integTests/tests/queue_tuning/dedicated_master/QueueDeciderDedicatedMasterITest.java
@@ -98,6 +98,8 @@ public class QueueDeciderDedicatedMasterITest {
       reason = "A unit test expressly calls SubscribeResponseHandler#onError, which writes an error log")
   @AErrorPatternIgnored(pattern = "NodeConfigCacheReaderUtil:readQueueCapacity()",
       reason = "Metrics is expected to be missing")
+  @AErrorPatternIgnored(pattern = "ModifyQueueCapacityAction:build()",
+      reason = "Metrics is expected to be missing")
   public void testQueueCapacityDecider() {
   }
 
