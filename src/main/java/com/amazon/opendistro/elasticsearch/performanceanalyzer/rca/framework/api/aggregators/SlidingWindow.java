ee67a171f5ec7ca3401189dd7828f5d14d394726
Aditya Jindal <aditjind@amazon.com>
2020-10-14 16:35:54 -0700

    Fixing SpotBugs Failures

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/api/aggregators/SlidingWindow.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/api/aggregators/SlidingWindow.java
@@ -100,4 +100,8 @@ public class SlidingWindow<E extends SlidingWindowData> {
   public int size() {
     return windowDeque.size();
   }
+
+  public TimeUnit readTimeUnit() {
+    return this.timeUnit;
+  }
 }
