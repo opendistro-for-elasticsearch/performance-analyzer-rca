4286dbda1a93a6528debf772d754361d9edd95ef
Aditya Jindal <aditjind@amazon.com>
2020-10-29 19:10:30 -0700

    Updating

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/decisionmaker/actions/CacheClearAction.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/decisionmaker/actions/CacheClearAction.java
@@ -131,10 +131,10 @@ public class CacheClearAction extends SuppressibleAction {
     @SerializedName(value = IP)
     private String[] ip;
     @SerializedName(value = COOL_OFF_PERIOD)
-//    @SuppressFBWarnings(value = "URF_UNREAD_FIELD", justification = "Used in Summary Serializing")
+    @SuppressFBWarnings(value = "URF_UNREAD_FIELD", justification = "Used in Summary Serializing")
     private long coolOffPeriodInMillis;
     @SerializedName(value = CAN_UPDATE)
-//    @SuppressFBWarnings(value = "URF_UNREAD_FIELD", justification = "Used in Summary Serializing")
+    @SuppressFBWarnings(value = "URF_UNREAD_FIELD", justification = "Used in Summary Serializing")
     private boolean canUpdate;
 
     public Summary(final List<NodeKey> impactedNodes,
