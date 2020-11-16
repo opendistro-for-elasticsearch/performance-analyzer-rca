3c76bd78ec3358155a1e0e475beec72c62632d4d
Sid Narayan <sidnaray@amazon.com>
2020-11-16 13:28:42 -0800

    Add PersistableSlidingWindow UT for multiple writes

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/api/aggregators/PersistableSlidingWindowTest.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/api/aggregators/PersistableSlidingWindowTest.java
@@ -84,7 +84,21 @@ public class PersistableSlidingWindowTest {
     Assert.assertEquals(1, slidingWindow.size());
   }
 
-
+  /**
+   * Tests that a PSW can call write() multiple times and still have its data read
+   */
+  @Test
+  public void testMultipleWrites() throws IOException {
+    PersistableSlidingWindow slidingWindow = new PersistableSlidingWindow(1, TimeUnit.SECONDS, persistFile);
+    long curTimestamp = Instant.now().toEpochMilli();
+    slidingWindow.next(new SlidingWindowData(curTimestamp, 10));
+    slidingWindow.write();
+    // This call to next removes the previous data from the window
+    slidingWindow.next(new SlidingWindowData(curTimestamp, 10));
+    slidingWindow.write();
+    PersistableSlidingWindow slidingWindow2 = new PersistableSlidingWindow(1, TimeUnit.SECONDS, persistFile);
+    Assert.assertEquals(20, slidingWindow2.readSum(), 0.0);
+  }
 
   @AfterClass
   public static void tearDown() {
