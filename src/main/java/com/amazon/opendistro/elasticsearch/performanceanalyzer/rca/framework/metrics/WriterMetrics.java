01f9b592d929e08922a9867b03ad7deb85125e3f
Yu Sun <sunayu@amazon.com>
2020-11-30 13:59:49 -0800

    only log count as statistics for stale_metrics

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/metrics/WriterMetrics.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/metrics/WriterMetrics.java
@@ -35,9 +35,7 @@ public enum WriterMetrics implements MeasurementSet {
     FAULT_DETECTION_COLLECTOR_EXECUTION_TIME("FaultDetectionCollectorExecutionTime", "millis", Arrays.asList(
     Statistics.MAX, Statistics.MIN, Statistics.MEAN, Statistics.COUNT, Statistics.SUM)),
 
-    STALE_METRICS("StaleMetrics", "count", Arrays.asList(
-        Statistics.MAX, Statistics.MIN, Statistics.MEAN, Statistics.COUNT, Statistics.SUM)),
-
+    STALE_METRICS("StaleMetrics", "count", Arrays.asList( Statistics.COUNT)),
     ;
 
     /** What we want to appear as the metric name. */
