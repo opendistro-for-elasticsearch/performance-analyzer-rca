8143d6f2aad57b866d501784365efb53cc7a1333
Yu Sun <11154022+yu-sun-77@users.noreply.github.com>
2020-12-01 14:28:29 -0800

    add new enum to log stale metrics count (#520)
    * add new enum to log stale metrics count
    
    * only log count as statistics for stale_metrics
    
    * remove extra space

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/metrics/WriterMetrics.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/rca/framework/metrics/WriterMetrics.java
@@ -33,7 +33,10 @@ public enum WriterMetrics implements MeasurementSet {
             Statistics.MAX, Statistics.MIN, Statistics.MEAN, Statistics.COUNT, Statistics.SUM)),
 
     FAULT_DETECTION_COLLECTOR_EXECUTION_TIME("FaultDetectionCollectorExecutionTime", "millis", Arrays.asList(
-    Statistics.MAX, Statistics.MIN, Statistics.MEAN, Statistics.COUNT, Statistics.SUM));
+    Statistics.MAX, Statistics.MIN, Statistics.MEAN, Statistics.COUNT, Statistics.SUM)),
+
+    STALE_METRICS("StaleMetrics", "count", Arrays.asList(Statistics.COUNT)),
+    ;
 
     /** What we want to appear as the metric name. */
     private String name;
