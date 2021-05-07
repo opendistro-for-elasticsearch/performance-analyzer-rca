62d66140705a981b344b99c32fd36c02f18c4727
Harold Xiao <haoruxia@amazon.com>
2021-05-06 17:17:34 -0700

    fix format issue

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/decisionmaker/deciders/jvm/JvmGenTuningPolicyTest.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/decisionmaker/deciders/jvm/JvmGenTuningPolicyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -52,139 +52,143 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class JvmGenTuningPolicyTest {
-    private JvmGenTuningPolicy policy;
-    @Mock private HighHeapUsageClusterRca rca;
-    @Mock private JvmGenTuningPolicyConfig policyConfig;
-    @Mock private JvmActionsAlarmMonitor tooSmallAlarm;
-    @Mock private JvmActionsAlarmMonitor tooLargeAlarm;
-    @Mock private RcaConf rcaConf;
-    @Mock private AppContext appContext;
+  private JvmGenTuningPolicy policy;
+  @Mock
+  private HighHeapUsageClusterRca rca;
+  @Mock
+  private JvmGenTuningPolicyConfig policyConfig;
+  @Mock
+  private JvmActionsAlarmMonitor tooSmallAlarm;
+  @Mock
+  private JvmActionsAlarmMonitor tooLargeAlarm;
+  @Mock
+  private RcaConf rcaConf;
+  @Mock
+  private AppContext appContext;
 
-    @Before
-    @SuppressWarnings("unchecked")
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        policy = new JvmGenTuningPolicy(rca, tooSmallAlarm, tooLargeAlarm);
-        policy.setRcaConf(rcaConf);
-        policy.setAppContext(appContext);
-        DeciderConfig deciderConfig = mock(DeciderConfig.class);
-        when(rcaConf.getDeciderConfig()).thenReturn(deciderConfig);
-        when(deciderConfig.getJvmGenTuningPolicyConfig()).thenReturn(policyConfig);
-        ResourceFlowUnit<HotClusterSummary> flowUnit =
-                (ResourceFlowUnit<HotClusterSummary>) mock(ResourceFlowUnit.class);
-        when(rca.getFlowUnits()).thenReturn(Collections.singletonList(flowUnit));
-    }
+  @Before
+  @SuppressWarnings("unchecked")
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    policy = new JvmGenTuningPolicy(rca, tooSmallAlarm, tooLargeAlarm);
+    policy.setRcaConf(rcaConf);
+    policy.setAppContext(appContext);
+    DeciderConfig deciderConfig = mock(DeciderConfig.class);
+    when(rcaConf.getDeciderConfig()).thenReturn(deciderConfig);
+    when(deciderConfig.getJvmGenTuningPolicyConfig()).thenReturn(policyConfig);
+    ResourceFlowUnit<HotClusterSummary> flowUnit = (ResourceFlowUnit<HotClusterSummary>) mock(ResourceFlowUnit.class);
+    when(rca.getFlowUnits()).thenReturn(Collections.singletonList(flowUnit));
+  }
 
-    /**
-     * After a call to this function, rca will return a FlowUnit containing n issues with resource
-     *
-     * <p>This is useful because the policy suggests actions based on resources and the count of
-     * issues observed for those resources.
-     *
-     * @param n the number of young gen issues
-     */
-    private void mockRcaIssues(int n, Resource resource) {
-        ResourceFlowUnit<HotClusterSummary> flowUnit = new ResourceFlowUnit<>(1);
-        HotNodeSummary nodeSummary = new HotNodeSummary(new Id("A"), new Ip("127.0.0.1"));
-        for (int i = 0; i < n; i++) {
-            nodeSummary.appendNestedSummary(new HotResourceSummary(resource, 1, 1, 1));
-        }
-        HotClusterSummary hotClusterSummary = new HotClusterSummary(3, 1);
-        hotClusterSummary.appendNestedSummary(nodeSummary);
-        flowUnit.setSummary(hotClusterSummary);
-        when(rca.getFlowUnits()).thenReturn(Collections.singletonList(flowUnit));
+  /**
+   * After a call to this function, rca will return a FlowUnit containing n issues with resource
+   *
+   * <p>This is useful because the policy suggests actions based on resources and the count of issues
+   * observed for those resources.
+   *
+   * @param n the number of young gen issues
+   */
+  private void mockRcaIssues(int n, Resource resource) {
+    ResourceFlowUnit<HotClusterSummary> flowUnit = new ResourceFlowUnit<>(1);
+    HotNodeSummary nodeSummary = new HotNodeSummary(new Id("A"), new Ip("127.0.0.1"));
+    for (int i = 0; i < n; i++) {
+      nodeSummary.appendNestedSummary(new HotResourceSummary(resource, 1, 1, 1));
     }
+    HotClusterSummary hotClusterSummary = new HotClusterSummary(3, 1);
+    hotClusterSummary.appendNestedSummary(nodeSummary);
+    flowUnit.setSummary(hotClusterSummary);
+    when(rca.getFlowUnits()).thenReturn(Collections.singletonList(flowUnit));
+  }
 
-    /**
-     * Causes the policy to view the old:young gen size ratio as desiredRatio
-     *
-     * @param desiredRatio the old:young gen size ratio; 3 means the old gen is 3X larger than young
-     */
-    private void mockCurrentRatio(double desiredRatio) {
-        NodeConfigCache cache = mock(NodeConfigCache.class);
-        when(appContext.getNodeConfigCache()).thenReturn(cache);
-        when(appContext.getDataNodeInstances())
-                .thenReturn(
-                        Collections.singletonList(
-                                new InstanceDetails(
-                                        NodeRole.DATA, new Id("A"), new Ip("127.0.0.1"), false)));
-        when(cache.get(any(), eq(ResourceUtil.OLD_GEN_MAX_SIZE))).thenReturn(desiredRatio);
-        when(cache.get(any(), eq(ResourceUtil.YOUNG_GEN_MAX_SIZE))).thenReturn(1d);
-    }
+  /**
+   * Causes the policy to view the old:young gen size ratio as desiredRatio
+   * @param desiredRatio the old:young gen size ratio; 3 means the old gen is 3X larger than young
+   */
+  private void mockCurrentRatio(double desiredRatio) {
+    NodeConfigCache cache = mock(NodeConfigCache.class);
+    when(appContext.getNodeConfigCache()).thenReturn(cache);
+    when(appContext.getDataNodeInstances()).thenReturn(Collections.singletonList(
+        new InstanceDetails(NodeRole.DATA, new Id("A"), new Ip("127.0.0.1"), false)));
+    when(cache.get(any(), eq(ResourceUtil.OLD_GEN_MAX_SIZE))).thenReturn(desiredRatio);
+    when(cache.get(any(), eq(ResourceUtil.YOUNG_GEN_MAX_SIZE))).thenReturn(1d);
+  }
 
-    /** When one of rcaConf, appContext is null, policy evaluation should return no actions */
-    @Test
-    public void testEvaluate_withoutContext() {
-        policy.setRcaConf(null);
-        Assert.assertTrue(policy.evaluate().isEmpty());
-        policy.setAppContext(null);
-        Assert.assertTrue(policy.evaluate().isEmpty());
-        policy.setRcaConf(rcaConf);
-        Assert.assertTrue(policy.evaluate().isEmpty());
-    }
+  /**
+   * When one of rcaConf, appContext is null, policy evaluation should return no actions
+   */
+  @Test
+  public void testEvaluate_withoutContext() {
+    policy.setRcaConf(null);
+    Assert.assertTrue(policy.evaluate().isEmpty());
+    policy.setAppContext(null);
+    Assert.assertTrue(policy.evaluate().isEmpty());
+    policy.setRcaConf(rcaConf);
+    Assert.assertTrue(policy.evaluate().isEmpty());
+  }
 
-    /** Tests that the evaluate() method returns the correct actions in various scenarios */
-    @Test
-    public void testEvaluate() {
-        // The policy should not emit actions when it is disabled
-        when(policyConfig.isEnabled()).thenReturn(false);
-        Assert.assertTrue(policy.evaluate().isEmpty());
-        when(policyConfig.isEnabled()).thenReturn(true);
-        // Neither generation has had enough issues
-        mockRcaIssues(1, MINOR_GC_PAUSE_TIME);
-        // The policy should not suggest any actions when there are no issues
-        when(policyConfig.allowYoungGenDownsize()).thenReturn(false);
-        Assert.assertTrue(policy.evaluate().isEmpty());
-        verify(tooLargeAlarm, times(1)).recordIssue();
-        reset(tooLargeAlarm);
-        // Make the young generation seem oversized
-        mockRcaIssues(10, MINOR_GC_PAUSE_TIME);
-        // The policy should not suggest decreasing young gen when the option is disabled
-        when(tooLargeAlarm.isHealthy()).thenReturn(false);
-        when(policyConfig.allowYoungGenDownsize()).thenReturn(false);
-        Assert.assertTrue(policy.evaluate().isEmpty());
-        verify(tooLargeAlarm, times(10)).recordIssue();
-        // The policy should suggest decreasing young gen when it is oversized and the option is
-        // enabled
-        when(policyConfig.allowYoungGenDownsize()).thenReturn(true);
-        mockCurrentRatio(4);
-        List<Action> actions = policy.evaluate();
-        Assert.assertEquals(1, actions.size());
-        Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
-        Assert.assertEquals(5, ((JvmGenAction) actions.get(0)).getTargetRatio());
-        mockCurrentRatio(5);
-        actions = policy.evaluate();
-        Assert.assertEquals(1, actions.size());
-        Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
-        Assert.assertEquals(6, ((JvmGenAction) actions.get(0)).getTargetRatio());
-        // Should not decrease the young gen size if the ratio is beyond 5:1
-        mockCurrentRatio(5.1);
-        actions = policy.evaluate();
-        Assert.assertTrue(actions.isEmpty());
-        // Make the young generation seem undersized
-        mockRcaIssues(10, YOUNG_GEN_PROMOTION_RATE);
-        // The policy should suggest increasing young gen when it is undersized
-        mockCurrentRatio(50);
-        when(tooLargeAlarm.isHealthy()).thenReturn(true);
-        when(tooSmallAlarm.isHealthy()).thenReturn(false);
-        actions = policy.evaluate();
-        verify(tooSmallAlarm, times(10)).recordIssue();
-        Assert.assertEquals(1, actions.size());
-        Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
-        Assert.assertEquals(3, ((JvmGenAction) actions.get(0)).getTargetRatio());
-        mockCurrentRatio(5);
-        actions = policy.evaluate();
-        Assert.assertEquals(1, actions.size());
-        Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
-        Assert.assertEquals(4, ((JvmGenAction) actions.get(0)).getTargetRatio());
-        mockCurrentRatio(4);
-        actions = policy.evaluate();
-        Assert.assertEquals(1, actions.size());
-        Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
-        Assert.assertEquals(3, ((JvmGenAction) actions.get(0)).getTargetRatio());
-        // Should not increase the young gen size if the ratio is not beyond 4:1
-        mockCurrentRatio(3.9);
-        actions = policy.evaluate();
-        Assert.assertTrue(actions.isEmpty());
-    }
+  /**
+   * Tests that the evaluate() method returns the correct actions in various scenarios
+   */
+  @Test
+  public void testEvaluate() {
+    // The policy should not emit actions when it is disabled
+    when(policyConfig.isEnabled()).thenReturn(false);
+    Assert.assertTrue(policy.evaluate().isEmpty());
+    when(policyConfig.isEnabled()).thenReturn(true);
+    // Neither generation has had enough issues
+    mockRcaIssues(1, MINOR_GC_PAUSE_TIME);
+    // The policy should not suggest any actions when there are no issues
+    when(policyConfig.allowYoungGenDownsize()).thenReturn(false);
+    Assert.assertTrue(policy.evaluate().isEmpty());
+    verify(tooLargeAlarm, times(1)).recordIssue();
+    reset(tooLargeAlarm);
+    // Make the young generation seem oversized
+    mockRcaIssues(10, MINOR_GC_PAUSE_TIME);
+    // The policy should not suggest decreasing young gen when the option is disabled
+    when(tooLargeAlarm.isHealthy()).thenReturn(false);
+    when(policyConfig.allowYoungGenDownsize()).thenReturn(false);
+    Assert.assertTrue(policy.evaluate().isEmpty());
+    verify(tooLargeAlarm, times(10)).recordIssue();
+    // The policy should suggest decreasing young gen when it is oversized and the option is enabled
+    when(policyConfig.allowYoungGenDownsize()).thenReturn(true);
+    mockCurrentRatio(4);
+    List<Action> actions = policy.evaluate();
+    Assert.assertEquals(1, actions.size());
+    Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
+    Assert.assertEquals(5, ((JvmGenAction) actions.get(0)).getTargetRatio());
+    mockCurrentRatio(5);
+    actions = policy.evaluate();
+    Assert.assertEquals(1, actions.size());
+    Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
+    Assert.assertEquals(6, ((JvmGenAction) actions.get(0)).getTargetRatio());
+    // Should not decrease the young gen size if the ratio is beyond 5:1
+    mockCurrentRatio(5.1);
+    actions = policy.evaluate();
+    Assert.assertTrue(actions.isEmpty());
+    // Make the young generation seem undersized
+    mockRcaIssues(10, YOUNG_GEN_PROMOTION_RATE);
+    // The policy should suggest increasing young gen when it is undersized
+    mockCurrentRatio(50);
+    when(tooLargeAlarm.isHealthy()).thenReturn(true);
+    when(tooSmallAlarm.isHealthy()).thenReturn(false);
+    actions = policy.evaluate();
+    verify(tooSmallAlarm, times(10)).recordIssue();
+    Assert.assertEquals(1, actions.size());
+    Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
+    Assert.assertEquals(3, ((JvmGenAction) actions.get(0)).getTargetRatio());
+    mockCurrentRatio(5);
+    actions = policy.evaluate();
+    Assert.assertEquals(1, actions.size());
+    Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
+    Assert.assertEquals(4, ((JvmGenAction) actions.get(0)).getTargetRatio());
+    mockCurrentRatio(4);
+    actions = policy.evaluate();
+    Assert.assertEquals(1, actions.size());
+    Assert.assertTrue(actions.get(0) instanceof JvmGenAction);
+    Assert.assertEquals(3, ((JvmGenAction) actions.get(0)).getTargetRatio());
+    // Should not increase the young gen size if the ratio is not beyond 4:1
+    mockCurrentRatio(3.9);
+    actions = policy.evaluate();
+    Assert.assertTrue(actions.isEmpty());
+  }
 }
